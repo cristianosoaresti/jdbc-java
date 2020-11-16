3e5f6e6ca31fd25b03234d0d18409154bc031794
Cristiano Soares <cristianosoaresti@hotmail.com>
2020-11-16 17:09:45 -0300

    Adding

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Book test");
-		System.out.println(dp.toString());
+		System.out.println(dp);
+		Seller seller = new Seller(1, "Seller Teste", "selleremail@teste.com.br", new Date() , 2000.00, dp);
+		System.out.println(seller);
 	}
 }
